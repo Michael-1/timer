236cb99eef9458b1fba43a825d81b62c9ee545fb
Michael Schmid <michael.schmid@live.com>
2020-01-08 14:10:12 +0100

    Dev server reachable from other hosts

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
+  mode: "development",
+  devServer: {
+    host: "0.0.0.0"
+  },
+  devtool: "source-map",
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /\/node_modules\//,
-      use: {
-        loader: 'babel-loader'
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /\/node_modules\//,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /\/node_modules\//,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
-    },{
-      test: /\.s?css$/,
-      exclude: /\/node_modules\//,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
+    ]
   }
-}
+};
