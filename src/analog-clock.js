8fcf17dd8e8acbb5beb562245b2339aa72a71816
Michael Schmid <michael.schmid@live.com>
2020-01-08 13:35:05 +0100

    Clock geometry around center

--- src/analog-clock.js
+++ src/analog-clock.js
@@ -1,5 +1,5 @@
 import m from "mithril";
-import { model } from "./model";
+import { model, STATE } from "./model";
 
 import "./analog-clock.scss";
 
@@ -22,90 +22,90 @@ export default {
       ticks.push(
         <line
           class={`tick ${majorTick ? "major" : ""}`}
-          x1={clockRadius}
-          y1={0}
-          x2={clockRadius}
-          y2={majorTick ? majorTickSize : minorTickSize}
-          transform={`rotate(${rotation},50,50)`}
+          x1={0}
+          y1={-clockRadius}
+          x2={0}
+          y2={-clockRadius + (majorTick ? majorTickSize : minorTickSize)}
+          transform={`rotate(${rotation},0,0)`}
         />
       );
       if (!majorTick) {
         continue;
       }
-      const rotationRad = (rotation / 180) * Math.PI;
+      const textPosition = polarToCartesian(
+        clockRadius - majorTickSize - labelFontSize / 2 - 3,
+        -(rotation / 180) * Math.PI
+      );
       ticks.push(
-        <text
-          x={
-            clockRadius +
-            (clockRadius - majorTickSize - labelFontSize / 2 - 2) *
-              Math.sin(rotationRad)
-          }
-          y={
-            clockRadius +
-            (-clockRadius + majorTickSize + labelFontSize / 2 + 2) *
-              Math.cos(rotationRad) +
-            1
-          }
-        >
+        <text x={textPosition.x} y={textPosition.y}>
           {time / tickUnit}
         </text>
       );
     }
-
+    const outerPositionOriginal = polarToCartesian(
+      clockRadius,
+      (model.originalTime / totalTime) * 2 * Math.PI
+    );
+    const innerPositionOriginal = polarToCartesian(
+      innerClockRadius,
+      (model.originalTime / totalTime) * 2 * Math.PI
+    );
+    const outerPositionLeft = polarToCartesian(
+      clockRadius,
+      (model.timeLeft / totalTime) * 2 * Math.PI
+    );
+    const innerPositionLeft = polarToCartesian(
+      innerClockRadius,
+      (model.timeLeft / totalTime) * 2 * Math.PI
+    );
     return (
       <svg
         id="analog-clock"
         viewBox={`0 0 ${clockRadius * 2} ${clockRadius * 2}`}
       >
-        <path
-          class="originalTime"
-          d={`
-            M
-            ${clockRadius} 0
-            A
-            ${clockRadius} ${clockRadius} 
-            0 0 0
-            ${clockRadius -
-              clockRadius *
-                Math.sin((model.originalTime / totalTime) * 2 * Math.PI)}
-            ${clockRadius -
-              clockRadius *
-                Math.cos((model.originalTime / totalTime) * 2 * Math.PI)}
-            L
-            ${clockRadius -
-              innerClockRadius *
-                Math.sin((model.originalTime / totalTime) * 2 * Math.PI)}
-            ${clockRadius -
-              innerClockRadius *
-                Math.cos((model.originalTime / totalTime) * 2 * Math.PI)}
-            A
-            ${innerClockRadius} ${innerClockRadius} 
-              0 0 1
-              ${clockRadius} ${clockRadius - innerClockRadius}
+        <g transform={`translate(${clockRadius} ${clockRadius})`}>
+          <path
+            class="originalTime"
+            d={`
+            M 0 ${-clockRadius}
+            A ${clockRadius} ${clockRadius} 0 0 0 ${outerPositionOriginal.x} ${
+              outerPositionOriginal.y
+            }
+            L ${innerPositionOriginal.x} ${innerPositionOriginal.y}
+            A ${innerClockRadius} ${innerClockRadius} 0 0 1 ${0} ${-innerClockRadius}
+            
           `}
-        />
-        <path
-          class="timeLeft"
-          d={`
-            M
-            ${clockRadius} 0
-            A
-            ${clockRadius} ${clockRadius} 0 0 0
-            ${clockRadius -
-              clockRadius *
-                Math.sin((model.timeLeft / totalTime) * 2 * Math.PI)}
-            ${clockRadius -
-              clockRadius *
-                Math.cos((model.timeLeft / totalTime) * 2 * Math.PI)}
-            L
-            ${clockRadius} ${clockRadius}
-            L
-            ${clockRadius} 0
+          />
+          <path
+            class="timeLeft"
+            d={`
+            M 0 ${-clockRadius}
+            A ${clockRadius} ${clockRadius} 0 0 0 ${outerPositionLeft.x} ${
+              outerPositionLeft.y
+            }
+            ${
+              model.state === STATE.PAUSED
+                ? `
+                L ${innerPositionLeft.x} ${innerPositionLeft.y}
+                A ${innerClockRadius} ${innerClockRadius} 0 0 1 ${0} ${-innerClockRadius}
+                `
+                : `
+                L 0 0
+                `
+            }
           `}
-        />
-        <circle cx={clockRadius} cy={clockRadius} r={1} />
-        {ticks}
+          />
+          <circle cx={0} cy={0} r={1} />
+          {ticks}
+        </g>
       </svg>
     );
   }
+};
+
+const polarToCartesian = function(radius, angle) {
+  return {
+    x: -radius * Math.sin(angle),
+    y: -radius * Math.cos(angle)
+  };
 };
