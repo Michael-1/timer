1fadd97ff373d0eeccbb2ec6b2bfa54123993630
Michael Schmid <michael.schmid@live.com>
2020-02-01 23:03:45 +0100

    Fix digital countdown

--- src/digital-clock.js
+++ src/digital-clock.js
@@ -10,9 +10,11 @@ const input = {
   view: function() {
     let time =
       model.state === STATE.READY ? model.originalTime : model.timeLeft;
-    const seconds = Math.round(time / 1000) % 60;
-    const minutes = Math.floor(time / (1000 * 60)) % 60;
-    const hours = Math.floor(time / (1000 * 60 * 60));
+    const milliseconds = time % 1000;
+    const seconds = ((time - milliseconds) / 1000) % 60;
+    const minutes = (((time - milliseconds) / 1000 - seconds) / 60) % 60;
+    const hours =
+      (((time - milliseconds) / 1000 - seconds) / 60 - minutes) / 60;
     let text, ghost;
     if (input.userInput === null) {
       const textHours = hours ? hours : "";
