f97b33511f7a3655d350a801a676bd67a5c52102
Michael Schmid <michael.schmid@live.com>
2020-04-01 23:09:20 +0200

    Replace social-tag plugin with template

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,8 +5,6 @@ const baseConfig = require("./webpack.common.js");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
-const SocialTags = require("social-tags-webpack-plugin");
-
 module.exports = merge(baseConfig, {
   mode: "production",
   output: {
@@ -14,12 +12,8 @@ module.exports = merge(baseConfig, {
   },
   plugins: [
     new HtmlWebpackPlugin({
+      template: "index.html",
       inject: "body",
-      title: "Timer",
-      meta: {
-        description: "A simple, beautiful timer for meetings and workshops",
-        viewport: "width=device-width"
-      },
       inlineSource: ".(js|css)$"
     }),
     new HtmlWebpackInlineSourcePlugin(),
@@ -35,21 +29,6 @@ module.exports = merge(baseConfig, {
           coast: false,
           yandex: false
         }
-      }
-    }),
-    new SocialTags({
-      appUrl: "https://timer.pizza/",
-      facebook: {
-        "og:title": "Pizza Timer",
-        "og:determiner": "the",
-        "og:type": "website",
-        "og:image": "./assets/og.png",
-        "og:image:alt": "Screenshot",
-        "og:image:width": "3600",
-        "og:image:height": "1881",
-        "og:description":
-          "A simple, beautiful timer for meetings and workshops",
-        "og:url": "https://timer.pizza"
       }
     })
   ]
