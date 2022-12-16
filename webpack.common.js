9f1b8ee31542f994cfd82544c678bdcfcf5a47b4
Michael Schmid <michael.schmid@live.com>
2022-12-16 21:33:50 +0100

    Update SASS

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const MiniCssExtractPlugin = require("mini-css-extract-plugin").default;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   plugins: [new MiniCssExtractPlugin()],
